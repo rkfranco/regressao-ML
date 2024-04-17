eecbfa717e1cbfdb8127603c6710137006f2e6cf
Rodrigo Kapulka Franco <rkfranco@furb.br>
2024-04-17 20:51:51 -0300

    Ajustes no grafico e regressao

--- main.py
+++ main.py
@@ -21,23 +21,40 @@ def exibir_grafico(x_treino, y_treino, x_test, y_test, title):
      regressao_tres,
      regressao_oito) = obter_regressao(x_test, coeficiente_um, coeficiente_dois, coeficiente_tres, coeficiente_oito)
 
-    desenhar_grafico(y_test, regressao_um, regressao_dois, regressao_tres, regressao_oito)
+    desenhar_grafico(x_treino, y_test, regressao_um, regressao_dois, regressao_tres, regressao_oito)
     plt.legend()
     plt.show()
 
 
-def desenhar_grafico(y, l1, l2, l3, l8):
+def desenhar_grafico(x_treino, y, l1, l2, l3, l8):
     # TODO: VALIDAR SE O VALOR DO EQM E DO R2 ESTAO CORRETOS
-    plt.plot(l1, label='Grau 1' + get_legenda_eqm_r2(y, l1), color='red')
-    plt.plot(l2, label='Grau 2' + get_legenda_eqm_r2(y, l2), color='green')
-    plt.plot(l3, label='Grau 3' + get_legenda_eqm_r2(y, l3), color='black')
-    plt.plot(l8, label='Grau 8' + get_legenda_eqm_r2(y, l8), color='yellow')
+    plt.plot(x_treino, l1, label='Grau 1' + get_legenda_eqm_r2(y, l1), color='red')
+    plt.plot(x_treino, l2, label='Grau 2' + get_legenda_eqm_r2(y, l2), color='green')
+    plt.plot(x_treino, l3, label='Grau 3' + get_legenda_eqm_r2(y, l3), color='black')
+    plt.plot(x_treino, l8, label='Grau 8' + get_legenda_eqm_r2(y, l8), color='yellow')
 
 
 def obter_regressao(x, b1, b2, b3, b8):
     # TODO: Os coeficientes estão na ordem contraria / VALIDAR SE PRECISA FAZER ISSO
-    x_reverse = x[::-1]
-    return np.polyval(b1, x_reverse), np.polyval(b2, x_reverse), np.polyval(b3, x_reverse), np.polyval(b8, x_reverse)
+
+    y1 = []
+    y2 = []
+    y3 = []
+    y8 = []
+    for valor in x:
+        y1.append(calcular_regressao(valor, b1[::-1]))
+        y2.append(calcular_regressao(valor, b2[::-1]))
+        y3.append(calcular_regressao(valor, b3[::-1]))
+        y8.append(calcular_regressao(valor, b8[::-1]))
+
+    return y1, y2, y3, y8
+
+
+def calcular_regressao(x, beta):
+    soma = beta[0]
+    for i in range(1, len(beta)):
+        soma += beta[i] * (x ** i)
+    return soma
 
 
 def obter_coeficientes(x, y):
@@ -52,8 +69,8 @@ def calcular_eqm(yi, yf):
     # eqm - erro quadratico medio
     soma = 0
     for i in range(len(yf)):
-        soma += (yi[i] - yf[i]) ** 2 / (1 / (1 + i))
-    return soma
+        soma += (yi[i] - yf[i]) ** 2
+    return soma / len(yf)
 
 
 def dividir_dados_treino(x, y):
@@ -68,7 +85,8 @@ def dividir_dados_treino(x, y):
 def demo_regressaop():
     x_test, y_test, x_treinamento, y_treinamento = dividir_dados_treino(col_x, col_y)
     exibir_grafico(col_x, col_y, col_x, col_y, 'Gráfico de dispersão - Todos os dados')
-    exibir_grafico(x_treinamento, y_treinamento, x_test, y_test, 'Gráfico de dispersão - Divisão dos dados (Treinamento/Teste)')
+    exibir_grafico(x_treinamento, y_treinamento, x_test, y_test,
+                   'Gráfico de dispersão - Divisão dos dados (Treinamento/Teste)')
 
 
 if __name__ == '__main__':
